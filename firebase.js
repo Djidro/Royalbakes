c5e5facd4b463678cfd3a608f19ad976df13dfa0
nadjib <Nadjibharera@gmail.com>
2025-06-26 10:43:34 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -14,10 +14,7 @@ const firebaseConfig = {
   measurementId: "G-HS02HS12EZ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const analytics = getAnalytics(app);
-
-export { db, auth, analytics };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app); // Analytics initialized
